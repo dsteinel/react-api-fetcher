c88d7e67c6d1aabe475f6b2770dffa41f805123e
Dimitri Steinel <dimitri.steinel@moccu.com>
2017-03-12 21:19:19 +0100

    main: remove unused component inport

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,6 @@ import ReactDOM from 'react-dom';
 
 import Home from './pages/Home';
 
-// import Component from './components/Component';
-
 // Load up the application styles
 require('../scss/styles.scss');
 
