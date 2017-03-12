2a76101496a4f2997c04bf0431f16b91b92f9933
Dimitri Steinel <dimitri.steinel@moccu.com>
2017-03-12 21:14:09 +0100

    Searchbar: add text

--- src/js/components/searchengine/includes/Searchbar.js
+++ src/js/components/searchengine/includes/Searchbar.js
@@ -19,7 +19,7 @@ class Searchbar extends React.Component {
 		return (
 			<form class="search-bar active" onSbumit={ this._onSubmit.bind(this) }>
 				<input ref="searchInput" type="text" placeholder="Type to search" />
-				<button type="submit" value="Submit" onClick={ this._onSubmit.bind(this) }><span></span></button>
+				<button type="submit" value="Submit" onClick={ this._onSubmit.bind(this) }><span>Search</span></button>
 			</form>
 		);
 	}
